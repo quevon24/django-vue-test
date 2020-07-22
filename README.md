6564c63dcf144d149bfbe383f0acd1c5f0040be9
Kevin Ramirez <quevon24@gmail.com>
2020-07-22 11:05:18 -0500

    differente vuejs implementation

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 https://www.sitepoint.com/web-app-prototype-django-vue/
+
+https://auth0.com/blog/building-modern-applications-with-django-and-vuejs/
--- main/settings.py
+++ main/settings.py
@@ -35,6 +35,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework_jwt',
+    'corsheaders',
     'blog'
 ]
 
@@ -46,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'main.urls'
@@ -113,3 +117,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# DRF
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+    ),
+}
+
+CORS_ORIGIN_WHITELIST = (
+    'localhost:8080',
+)
